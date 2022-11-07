461005eabd4791349d0c2ac8b20be48851b21c6d
Carina C <cardozo1747@gmail.com>
2022-11-07 12:02:08 -0600

    caching DOM elements

--- rps.js
+++ rps.js
@@ -1,15 +1,17 @@
  // Rock Paper Scissors Game //    
 
-// Declare play variables 
-const play = ["rock", "paper", "scissors"];
-play = play.toLowerCase(); 
 
-// Declare both scores starting at 0 
-let computerSelection;
-let playerSelection; 
-let computerScore = 0;
-let playerScore = 0;
-let winner = '';
+// Caching DOM elements 
+const computerScore = 0;
+const playerScore = 0;
+const playerScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const results_div = document.querySelector(".results");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
 
 // Function to play computers turn 
 function computerSelection() {
@@ -56,5 +58,50 @@ function playRound(playerSelection, computerSelection) {
 
 // Function to keep score 
 function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = prompt("Lets play rock paper scissors!");
 
-}
+        console.log ("you choose " +playerSelection.toLowerCase()); 
+
+        computerSelection = ["rock","paper","scissors"];//update computerSelection
+        computerPlay= Math.floor(Math.random() * computerSelection.length);//update computerPlay
+  
+        console.log("The computer choose " +computerPlay, computerSelection[computerPlay]);//define computer input  
+
+        console.log(playRound(playerSelection, computerSelection)); 
+         }
+          if (playerScore>computerScore){
+             return "You Win this game.\n -------";
+           }
+             if (playerScore==computerScore){
+               return "This game is a tie.\n -------";
+             }
+            else{
+             return "You Lose this game.\n -------";
+           }
+        }
+        
+        console.log(game()); //play the loop
+        
+        console.log( "Your final score is " +playerScore +" wins " +computerScore +" loses and " +tieScore +" draws.\n -------");
+        
+        score=playerScore+computerScore+tieScore;
+        
+        console.log("You played this game " +score +" times.\n -------\nGame over.");
+        //count win lose or tie
+
+const playGame = () => {
+
+    const userChoice=getUserChoice('NUMNUM');
+  
+    const computerChoice=getComputerChoice();
+  
+  console.log(`Player choose ${userChoice}.
+  
+  Computer choose ${computerChoice}`);
+  
+   console.log(determineWinner(userChoice,computerChoice));
+  
+  };
+  
+  playGame();
